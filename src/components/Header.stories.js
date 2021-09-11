0151c162efcf37406999dd5b8f4d13dcc79e21df
kkata <14849535+kkata@users.noreply.github.com>
2021-09-11 19:28:37 +0900

    Actions addon の設定

---
+++ src/components/Header.stories.js
@@ -0,0 +1,25 @@
+import Header from "./Header";
+
+export default {
+  title: "Header",
+  component: Header,
+  parameters: { actions: { argTypesRegex: ".*" } },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Header },
+  template: `<Header @onLogout="onLogout"
+      @onLogin="onLogin"
+      @onSignUp="onSignUp" v-bind="$props" /> `,
+});
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  user: { id: 1, name: "Jobs" },
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {
+  user: {},
+};
