28345e8dac5256f43c364d0e6b8e0ab1ccf01c7a
kkata <14849535+kkata@users.noreply.github.com>
2021-09-12 16:59:47 +0900

    Toolbars & globals

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -37,17 +37,28 @@ export const parameters = {
 
 export const decorators = [
   (story, context) => ({
+    props: {
+      padding: {
+        type: String,
+        default: context.globals.padding,
+      },
+    },
     template: `
-      <div>
-        <h1>${context.kind}</h1>
-        <h2>args</h2>
-        <p>${JSON.stringify(context.args)}</p>
-        <h2>Result</h2>
-        <story />
-      </div>
-      <div style="backgroundColor: gray; padding: 10px">
+      <div :style="{padding: padding}">
         <story />
       </div>
     `,
   }),
 ];
+
+export const globalTypes = {
+  padding: {
+    name: "Padding",
+    description: "ストーリー描画時のpaddingを設定します",
+    defaultValue: "0px",
+    toolbar: {
+      icon: "circlehollow",
+      items: ["0px", "30px", "100px"],
+    },
+  },
+};
